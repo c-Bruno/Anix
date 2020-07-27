a3e060c5cfb2bf51b4574a4792fa0cdbb7a54442
c-Bruno <ribeiro.caboclo@gmail.com>
2020-07-27 18:48:21 -0300

    Desenvolvido primeiro modelo tela home

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/imgs/Logo.png'
+import './Menu.css'
+import Button from '../Button'
+//import ButtonLink from './componentes/ButtonLink'
+
+function Menu(){
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Anix logo"/>
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo epsodio
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
