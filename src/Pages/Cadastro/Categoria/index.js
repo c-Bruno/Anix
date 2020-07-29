8e3a2ca5086b733c878c1d3cce5a4ebbcd1b5627
c-Bruno <ribeiro.caboclo@gmail.com>
2020-07-29 19:58:55 -0300

    commit da aula 3 do imersão

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -1,25 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from '../../../componentes/PageDefault'
 import { Link } from 'react-router-dom';
+import FormField from '../../../componentes/FormField';
 
 
 function CadastroCategoria () {
+    
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '',
+    }
+
+    const [categorias, setCategorias] = useState([])
+    const [values, setValues] = useState(valoresIniciais)
+
+    function setValue(chave, valor) {
+        setValues({
+            ...values,
+            [chave]: valor,
+        })
+    }
+
+    function digitar(info) {
+        /* const { getAttribute, value } = info.target; */
+        setValue(
+            info.target.getAttribute('name'), 
+            info.target.value
+        )}
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {values.nome}</h1>
             
-            <form>
-                <label>
-                Nome da Categoria:
-                <input
+            <form onSubmit={function handleSubmit(info){
+                info.preventDefault()
+                
+                setCategorias([
+                    ...categorias, 
+                    values
+                ])
+
+                setValues(valoresIniciais)
+            }}>
+
+            <FormField
+                value={values.nome}
+                onChange={digitar}
+            />   
+
+            <div>
+            <label>
+                    Descrição:
+                <textarea
                     type="text"
+                    name="descricao"
+                    value = {values.descricao}
+                    onChange={digitar} 
                 />
                 </label>
+            </div>
+
+            <div>
+            <label>
+                    Cor:
+                <input
+                    type="color"
+                    name="cor"
+                    value = {values.cor}
+                    onChange = {digitar} 
+                />
+                </label>
+            </div>
 
                 <button>
                 Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria, indice) => {
+                    return (
+                        <li key={`${categoria} ${indice}`}>
+                            {categoria.nome}
+                        </li>
+
+                        
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para a home
